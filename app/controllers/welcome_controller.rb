b4e383a2b300773ba4e2fbc77ca77d1f66008782
Jaime <jpanaia@gmail.com>
2014-05-27 08:25:03 -0400

    updates

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,23 +4,23 @@ class WelcomeController < ApplicationController
   require 'open-uri'
   require 'json'
 
- #  def home
-	# @results = JSON.parse(open("https://gtmp.linkssystem.org/api/districts").read)
+  def home
+	@results = JSON.parse(open("https://gtmp.linkssystem.org/api/districts").read)
 	  	
- #  end
-
- #  def samplejson
- #  	@results = JSON.parse(open("https://gtmp.linkssystem.org/api/districts").read)	  
- #  end
-
-    def home
-	@status = JSON.parse(open("https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&sensor=false&key=AIzaSyAKqlF67qdx09dtv7BpiEh12zv7ohlt5xQ
-").read)
   end
 
   def samplejson
-  	@status = JSON.parse(open("https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&sensor=false&key=AIzaSyAKqlF67qdx09dtv7BpiEh12zv7ohlt5xQ
-").read)	  
+  	@results = JSON.parse(open("https://gtmp.linkssystem.org/api/districts").read)	  
   end
+
+#     def home
+# 	@status = JSON.parse(open("https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&sensor=false&key=AIzaSyAKqlF67qdx09dtv7BpiEh12zv7ohlt5xQ
+# ").read)
+#   end
+
+#   def samplejson
+#   	@status = JSON.parse(open("https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&sensor=false&key=AIzaSyAKqlF67qdx09dtv7BpiEh12zv7ohlt5xQ
+# ").read)	  
+#   end
 
 end
